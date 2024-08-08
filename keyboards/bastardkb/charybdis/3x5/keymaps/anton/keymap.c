fdb1ccd658620ebced1863ade48f7aaf45c8baff
anton-jh <anton.jh@hotmail.com>
2024-08-08 21:06:15 +0200

    disable auto mouse layer timeout

--- keyboards/bastardkb/charybdis/3x5/keymaps/anton/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/anton/keymap.c
@@ -33,7 +33,7 @@ enum charybdis_keymap_layers {
 // #define CHARYBDIS_AUTO_SNIPING_ON_LAYER LAYER_POINTER
 
 #ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-static uint16_t auto_pointer_layer_timer = 0;
+//static uint16_t auto_pointer_layer_timer = 0;
 
 #    ifndef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS
 #        define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS 1000
@@ -48,6 +48,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define SPC_NUM LT(LAYER_NUMERAL, KC_SPC)
 #define TAB_FUN LT(LAYER_FUNCTION, KC_TAB)
 #define BSP_SYM LT(LAYER_SYMBOLS, KC_BSPC)
+#define ESC_PNT TG(LAYER_POINTER)
 // #define _L_PTR(KC) LT(LAYER_POINTER, KC)
 
 #ifndef POINTING_DEVICE_ENABLE
@@ -81,7 +82,7 @@ static uint16_t auto_pointer_layer_timer = 0;
     _______________DEAD_HALF_ROW_______________, S_D_MOD, DPI_MOD, XXXXXXX, XXXXXXX, XXXXXXX, \
     ______________HOME_ROW_GASC_L______________, XXXXXXX, XXXXXXX, KC_BTN3, XXXXXXX, XXXXXXX, \
     _______________DEAD_HALF_ROW_______________, XXXXXXX, KC_BTN1, DRGSCRL, KC_BTN2, SNIPING, \
-                      _______, _______, _______, _______, _______
+                      ESC_PNT, _______, _______, _______, _______
 
 #define LAYOUT_LAYER_NAVIGATION                                                               \
     XXXXXXX, KC_HOME,   KC_UP,  KC_END, XXXXXXX, _______________DEAD_HALF_ROW_______________, \
@@ -135,27 +136,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #    ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
     if (abs(mouse_report.x) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD || abs(mouse_report.y) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD) {
-        if (auto_pointer_layer_timer == 0) {
-            layer_on(LAYER_POINTER);
-#        ifdef RGB_MATRIX_ENABLE
-            rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
-            rgb_matrix_sethsv_noeeprom(HSV_GREEN);
-#        endif // RGB_MATRIX_ENABLE
-        }
-        auto_pointer_layer_timer = timer_read();
+        layer_on(LAYER_POINTER);
+//         if (auto_pointer_layer_timer == 0) {
+//             layer_on(LAYER_POINTER);
+// #        ifdef RGB_MATRIX_ENABLE
+//             rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
+//             rgb_matrix_sethsv_noeeprom(HSV_GREEN);
+// #        endif // RGB_MATRIX_ENABLE
+//         }
+//         auto_pointer_layer_timer = timer_read();
     }
     return mouse_report;
 }
 
-void matrix_scan_user(void) {
-    if (auto_pointer_layer_timer != 0 && TIMER_DIFF_16(timer_read(), auto_pointer_layer_timer) >= CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS) {
-        auto_pointer_layer_timer = 0;
-        layer_off(LAYER_POINTER);
-#        ifdef RGB_MATRIX_ENABLE
-        rgb_matrix_mode_noeeprom(RGB_MATRIX_DEFAULT_MODE);
-#        endif // RGB_MATRIX_ENABLE
-    }
-}
+// void matrix_scan_user(void) {
+//     if (auto_pointer_layer_timer != 0 && TIMER_DIFF_16(timer_read(), auto_pointer_layer_timer) >= CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS) {
+//         auto_pointer_layer_timer = 0;
+//         layer_off(LAYER_POINTER);
+// #        ifdef RGB_MATRIX_ENABLE
+//         rgb_matrix_mode_noeeprom(RGB_MATRIX_DEFAULT_MODE);
+// #        endif // RGB_MATRIX_ENABLE
+//     }
+// }
 #    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 
 #    ifdef CHARYBDIS_AUTO_SNIPING_ON_LAYER
