69ad227f73def4667a12e90911853148108f001c
anton-jh <anton.jh@hotmail.com>
2024-08-12 19:44:58 +0200

    Added keypad keys to caps word

--- keyboards/bastardkb/charybdis/3x5/keymaps/anton/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/anton/keymap.c
@@ -348,6 +348,7 @@ bool caps_word_press_user(uint16_t keycode) {
             add_weak_mods(MOD_BIT(KC_LSFT));
             return true;
         case KC_1 ... KC_0:
+        case KC_KP_1 ... KC_KP_0:
         case KC_BSPC:
         case KC_DEL:
             return true;
