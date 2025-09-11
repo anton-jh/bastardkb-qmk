4911a05422b923f2158307bd45fa0b9b3286cf45
anton-jh <anton.jh@hotmail.com>
2025-09-11 13:37:13 +0200

    remove mouse4 and 5 from nav layer

--- keyboards/bastardkb/charybdis/3x5/keymaps/anton/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/anton/keymap.c
@@ -190,7 +190,7 @@ enum custom_keycodes {
 #define LAYOUT_LAYER_NAVIGATION                                                               \
     KC_PGUP, KC_HOME,   KC_UP,  KC_END,  KC_INS, _______________DEAD_HALF_ROW_______________, \
     KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, ______________HOME_ROW_GASC_R______________, \
-    KC_BTN4, KC_MPRV, KC_MPLY, KC_MNXT, KC_BTN5, _______________DEAD_HALF_ROW_______________, \
+    XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, _______________DEAD_HALF_ROW_______________, \
                       _______, _______, _______,  KC_ENT, KC_BSPC
 
 #define LAYOUT_LAYER_NUMERAL                                                                  \
